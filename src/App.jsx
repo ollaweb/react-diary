a91c22e424ca522cbb2a38826fab7510bc46099c
Olga L <huzijm@yandex.ru>
2024-03-26 17:26:34 +0300

    feature: added showing message that there are no posts if there are no posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react';
 import './App.css';
-import CardButton from './components/CardButton/CardButton';
 import Header from './components/Header/Header';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton';
 import JournalForm from './components/JournalForm/JournalForm';
-import JournalItem from './components/JournalItem/JournalItem';
 import JournalList from './components/JournalList/JournalList';
 import Body from './layouts/Body/Body';
 import LeftPanel from './layouts/LeftPanel/LeftPanel';
@@ -12,20 +10,20 @@ import LeftPanel from './layouts/LeftPanel/LeftPanel';
 function App() {
 
 	const INITIAL_POSTS = [
-		{
-			id:1,
-			title: 'Высадка семян на рассаду',
-			date: new Date(),
-			tag: 'Огород',
-			text: 'Сегодня я начала делать посадки семян на рассаду. Поскольку уже настали благоприятные дни.'
-		},
-		{
-			id:2,
-			title: 'Пикировка томатов',
-			date: new Date(),
-			tag: 'Огород',
-			text: 'Как правильно выполнять пикирование томатов.'
-		}
+		// {
+		// 	id:1,
+		// 	title: 'Высадка семян на рассаду',
+		// 	date: new Date(),
+		// 	tag: 'Огород',
+		// 	text: 'Сегодня я начала делать посадки семян на рассаду. Поскольку уже настали благоприятные дни.'
+		// },
+		// {
+		// 	id:2,
+		// 	title: 'Пикировка томатов',
+		// 	date: new Date(),
+		// 	tag: 'Огород',
+		// 	text: 'Как правильно выполнять пикирование томатов.'
+		// }
 	];
 
 	const [posts, setPosts] = useState(INITIAL_POSTS);
@@ -40,30 +38,12 @@ function App() {
 		}]);
 	}
 
-	const sortPosts = (a, b) => {
-		if (a.date < b.date) {
-			return 1;
-		} else {
-			return -1;
-		}
-	};
-
 	return (
 		<div className='app'>
 			<LeftPanel>
 				<Header/>
 				<JournalAddButton/>
-				<JournalList>
-					{posts.sort(sortPosts).map(el => (
-						<CardButton key={el.id}>
-							<JournalItem
-								title={el.title}
-								date={el.date}
-								text={el.text}
-							/>
-						</CardButton>
-					))}
-				</JournalList>
+				<JournalList posts={posts}/>
 			</LeftPanel>
 			<Body>
 				<JournalForm onSubmit={addPost}/>
--- src/components/JournalList/JournalList.jsx
+++ src/components/JournalList/JournalList.jsx
@@ -1,9 +1,31 @@
 import './JournalList.css';
+import JournalItem from '../JournalItem/JournalItem';
+import CardButton from '../CardButton/CardButton';
 
-function JournalList({children}) {
+function JournalList({posts}) {
+
+	if (posts.length === 0) {
+		return <p>Записей пока нет. Добавьте первую запись</p>;
+	}
+
+	const sortPosts = (a, b) => {
+		if (a.date < b.date) {
+			return 1;
+		} else {
+			return -1;
+		}
+	};
 
 	return (
-		<div className='journal-list'>{children}</div>
+		<div className='journal-list'>{posts.sort(sortPosts).map(el => (
+			<CardButton key={el.id}>
+				<JournalItem
+					title={el.title}
+					date={el.date}
+					text={el.text}
+				/>
+			</CardButton>
+		))}</div>
 	);
 }
 
