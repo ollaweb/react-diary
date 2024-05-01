a9155ff484ecae7834c29887b8647e9d6be9e4c2
Olga L <huzijm@yandex.ru>
2024-05-02 02:05:36 +0300

    feature: added post validation with css clues

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,23 +1,54 @@
 import './JournalForm.css';
 import Button from '../Button/Button';
+import { useState } from 'react';
+
 
 function JournalForm({onSubmit}) {
 
-	const addJournalItem = (event) => {
-		event.preventDefault();
-		const formData = new FormData(event.target);
+	const [formValidState, setFormValidState] = useState({
+		title: true,
+		date: true,
+		tag: true,
+		text: true
+	});
+
+	const addJournalItem = (e) => {
+		e.preventDefault();
+		const formData = new FormData(e.target);
 		const formProps = Object.fromEntries(formData);
+		let isFormValid = true;
+		if (!formProps.title?.trim().length) {
+			setFormValidState(state => ({...state, title: false}));
+			isFormValid = false;
+		} else {
+			setFormValidState(state => ({...state, title: true}));
+		}
+		if (!formProps.text?.trim().length) {
+			setFormValidState(state => ({...state, text: false}));
+			isFormValid = false;
+		} else {
+			setFormValidState(state => ({...state, text: true}));
+		}
+		if (!formProps.date) {
+			setFormValidState(state => ({...state, date: false}));
+			isFormValid = false;
+		} else {
+			setFormValidState(state => ({...state, date: true}));
+		}
+		if (!isFormValid) {
+			return;
+		}
 		onSubmit(formProps);
-		event.target.reset();
+		e.target.reset();
 	};
 
 
 	return (
 		<form action="" className='journal-form' onSubmit={addJournalItem}>
-			<input type="text" name='title'/>
-			<input type="date" name="date"/>
+			<input type="text" name='title' className={`input ${formValidState.title ? '' : 'invalid'}`}/>
+			<input type="date" name="date" className={`input ${formValidState.date ? '' : 'invalid'}`}/>
 			<input type="text" name='tag'/>
-			<textarea name="text" id="" cols="30" rows="10"></textarea>
+			<textarea name="text" id="" cols="30" rows="10" className={`input ${formValidState.text ? '' : 'invalid'}`}></textarea>
 			<Button text='Сохранить'></Button>
 		</form>
 	);
